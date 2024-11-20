6446c7fc203b16bdf9dba6c850aeaa18cf9a84ef
hayleuser <hailyemisganew@gmail.com>
2024-11-19 16:43:07 -0900

    row

--- netflix-clone/src/Components/Header/Header.jsx
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Header.css";
 import NetLogo from "../../assets/logo.png";
 import SearchIcon from "@mui/icons-material/Search";
@@ -7,15 +7,26 @@ import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function Header() {
+
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll",handleShow);
+    };
+  }, []);
   return (
-    <div className="header_outer">
+    <div className={`header_outer ${show && "nav__black"}`}>
       <div className="header_inner">
         <div className="header_left">
           <ul className="headerUL">
             <li>
               <img src={NetLogo} alt="Netlix_logo" width={100} />
             </li>
-            <li>Netflix</li>
             <li>Home</li>
             <li>TVShows</li>
             <li>Movies</li>
